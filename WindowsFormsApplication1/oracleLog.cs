9dff8e5e5f8978a112e5745589fd8622845a3a89
nelss95 <nelsk895@gmail.com>
2015-10-11 05:55:47 -0600

    Conexion cdb de oracle
    Hay que instalar lo que puso liza ahora

--- WindowsFormsApplication1/oracleLog.cs
+++ WindowsFormsApplication1/oracleLog.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Oracle.DataAccess.Client;
 using Oracle.ManagedDataAccess;
-using Oracle.Web;
+//using Oracle.Web;
 using System.Data.OleDb;
 
 namespace WindowsFormsApplication1
@@ -33,23 +33,43 @@ namespace WindowsFormsApplication1
 
         private void entrar_Click(object sender, EventArgs e)
         {
+            //NomUsuario.Text
+            //password.Text
 
-            //se crea el string connection para pasarle la información de la bd al OracleConnection
-            string oradb = "Data Source=basesdos;User Id=hr;Password=hr;";
-            OracleConnection conn = new OracleConnection(oradb);
+            string user = NomUsuario.Text;//Request["user"];
+            string contrasennia = password.Text;//Request["password"];
+            string database = "pdbTest";//Request["database"];
+            //Session["user"] = user;
+            //Session["passwordOracle"] = password;
+            //Session["databaseOracle"] = database;
 
-            // La conexión es guardada en la variable myConnection para luego ser evaluada
-            OleDbConnection myConnection = new OleDbConnection(oradb);
-            try
-            {
+            String sConnectionString =
+             "Provider=MSDAORA.1; User ID=" + user + ";password=" + contrasennia + "; Data Source=" + database + ";Persist Security Info=False";
+
+            OleDbConnection myConnection = new OleDbConnection(sConnectionString);
+
                 myConnection.Open();
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("ta malo");
-            }
+                MessageBox.Show("Si hubo conexión :)");
 
-        }
+
+                //se crea el string connection para pasarle la información de la bd al OracleConnection
+                // string oradb = "Data Source=basesdos;User Id=hr;Password=hr;";
+                // OracleConnection conn = new OracleConnection(oradb);
+
+                // La conexión es guardada en la variable myConnection para luego ser evaluada
+                // OleDbConnection myConnection = new OleDbConnection(oradb);
+                /**
+                try
+                {
+                    myConnection.Open();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("ta malo");
+                }
+        **/
+
+            }
 
         private void password_TextChanged(object sender, EventArgs e)
         {
